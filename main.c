14c15eb7ed889c1a6b930c7cf9ab90261273a702
sencertekdas <stekdas@gmail.com>
2017-07-14 01:44:13 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <dirent.h>
+#include <sys/syscall>
+
+void create(String name){
+    
+}
+
+void listFiles(){
+    
+}
+
+void extractFiles(){
+    
+}
+
+void changeDir(){
+    
+}
+
+void archive(){
+    
+}
+
+
 
 int main()
 {
